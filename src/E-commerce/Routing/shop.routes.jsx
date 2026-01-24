b9d2df7b296fc864fd721bd1aff15420c10fa747
KhaledHemdan <136645522+KhaledHemdan@users.noreply.github.com>
2026-01-25 01:09:03 +0200

    Delete src directory

--- src/E-commerce/Routing/shop.routes.jsx
+++
@@ -1,9 +0,0 @@
-import { lazy } from "react";
-
-const ShopHome = lazy(() => import("@/E-commerce/Views/Shop/Home"));
-const ProductDetails = lazy(() => import("@/E-commerce/Views/Shop/ProductDetails"));
-
-export const shopRoutes = [
-  { index: true, element: <ShopHome /> },
-  { path: "product/:id", element: <ProductDetails /> },
-];
