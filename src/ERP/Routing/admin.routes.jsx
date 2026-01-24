0c57efe20a9300c297314ee794e96798124956f9
KhaledHemdan <136645522+KhaledHemdan@users.noreply.github.com>
2026-01-25 00:59:33 +0200

    Delete src directory

--- src/ERP/Routing/admin.routes.jsx
+++
@@ -1,9 +0,0 @@
-import { lazy } from "react";
-
-const AdminDashboard = lazy(() => import("@/ERP/Views/AdminDashboard"));
-const Users = lazy(() => import("@/ERP/Views/Users"));
-
-export const adminRoutes = [
-  { path: "dashboard", element: <AdminDashboard /> },
-  { path: "users", element: <Users /> },
-];
