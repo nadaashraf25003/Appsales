b86ed0bc78848d1c281a223162623deeb341b001
KhaledHemdan <136645522+KhaledHemdan@users.noreply.github.com>
2026-01-25 01:28:44 +0200

    Delete src directory

--- src/E-commerce/Routing/auth.routes.jsx
+++
@@ -1,9 +0,0 @@
-import { lazy } from "react";
-
-const Login = lazy(() => import("@/E-commerce/Views/Auth/Login"));
-const Register = lazy(() => import("@/E-commerce/Views/Auth/Register"));
-
-export const ecommerceAuthRoutes = [
-  { path: "login", element: <Login /> },
-  { path: "register", element: <Register /> },
-];
