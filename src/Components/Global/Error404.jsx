62f7a83f13759d80438a57bd5e3e8f1b2f508043
nada <nadanadaashraf25@gmail.com>
2026-01-25 12:41:36 +0200

    Updating

--- src/Components/Global/Error404.jsx
+++ src/Components/Global/Error404.jsx
@@ -1,5 +1,9 @@
-function Error404() {
-  return <></>;
-}
+const Error404 = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">404 | Page Not Found</h1>
+    </div>
+  );
+};
 
 export default Error404;
