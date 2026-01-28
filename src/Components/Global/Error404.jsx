7ea9296776a763ea92e49dc53a4c77751be145f5
KhaledHemdan <136645522+KhaledHemdan@users.noreply.github.com>
2026-01-28 15:14:46 +0200

    Delete src directory

--- src/Components/Global/Error404.jsx
+++
@@ -1,9 +0,0 @@
-const Error404 = () => {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">404 | Page Not Found</h1>
-    </div>
-  );
-};
-
-export default Error404;
--- src/E-commerce/Views/Shop/ShopLayout.jsx
+++
@@ -1,3 +0,0 @@
-export default function ShopLayout() {
-  return <div>Content</div>;
-}
